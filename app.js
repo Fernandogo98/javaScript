fd10c59357dcf0a66aac584154e2f7d0d1299ff3
Fernandogo98 <nandojunior34@gmail.com>
2025-04-11 19:46:48 -0300

    Change 3

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
-alert("Boas Vindas ao Site!");
+alert("Boas Vindas ao Site!"); //Alert Inicial
+//Variaveis Definidas
 let nome = "Lua";
 let idade = 25;
 let numeroDeVendas = 50;
@@ -6,6 +7,7 @@ let saldoDisponivel = 1000;
 let mensagemErro = "Erro! Preencha Todos os Campos";
 alert(mensagemErro);
 
+//Função para mudar variavel nome e idade e mandar alerta conforme a mudança
 nome = prompt("Qual é Seu Nome e Idade?");
 idade = prompt("Qual é a Sua Idade?")
 console.log(nome, idade);
