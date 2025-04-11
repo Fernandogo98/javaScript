c81a1b64e2d288bf89b8d2ecc150a148478146a2
Fernandogo98 <nandojunior34@gmail.com>
2025-04-11 19:36:18 -0300

    V1

---
+++ app.js
@@ -0,0 +1,14 @@
+alert("Boas Vindas ao Site!");
+let nome = "Lua";
+let idade = 25;
+let numeroDeVendas = 50;
+let saldoDisponivel = 1000;
+let mensagemErro = "Erro! Preencha Todos os Campos";
+alert(mensagemErro);
+
+nome = prompt("Qual é Seu Nome e Idade?");
+idade = prompt("Qual é a Sua Idade?")
+console.log(nome, idade);
+if (idade >= 18){
+    alert("Vocé já pode tirar Habilitação!!");
+}
