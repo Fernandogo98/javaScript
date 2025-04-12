dba0c0b9ac8f76fcd3b18bcd6f34975b26626d45
Fernandogo98 <nandojunior34@gmail.com>
2025-04-12 13:20:57 -0300

    V4

--- app.js
+++ app.js
@@ -57,3 +57,37 @@ if (numero == numeroSorte){
     alert(`Você Errou o Numero da Sorte digitou: ${numero}`);
 }
 */
+
+/*Desafios
+Crie um contador que comece em 1 e vá até 10 usando um loop while. Mostre cada número.
+contador = 1;
+
+while (contador <= 10){
+  console.log(contador);
+  contador++ 
+}
+*/
+/*Crie um contador que começa em 10 e vá até 0 usando um loop while. Mostre cada número.
+contador = 10;
+
+while(contador >= 0) {
+    console.log(contador);
+    contador-- 
+}
+*/
+/*Crie um programa de contagem regressiva. Peça um número e conte deste número até 0, usando um loop while no console do navegador.
+let numero = prompt("Digite um Numero para contagem:");
+
+while (numero >= 0) {
+    console.log(`O numero esta em : ${numero}`);
+    numero--;
+}
+*/
+/*Crie um programa de contagem progressiva. Peça um número e conte de 0 até esse número, usando um loop while no console do navegador.
+numero = prompt("Digite um Numero para começar a contar ate 100");
+contador = 0
+
+while (numero >= contador) {
+    console.log(`O Numero é: ${contador}`);
+    contador++
+}*/
