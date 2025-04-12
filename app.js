3ad46e1a6649604766775ec0c8a5c669ec78547e
Fernandogo98 <nandojunior34@gmail.com>
2025-04-12 11:29:22 -0300

    V2- New Test

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+/*Aula 01 Desafios
 alert("Boas Vindas ao Site!"); //Alert Inicial
 //Variaveis Definidas
 let nome = "Lua";
@@ -15,4 +16,44 @@ if (idade >= 18){
     alert("Vocé já pode tirar Habilitação!!");
 }else{
     alert("Você é menor de idade!!")
+}*/
+
+/*Aula 02 Desafios 1 - Alura
+nome = prompt("Qual é seu Nome?");
+alert(`Bem-Vindo a Experiencia de programação: ${nome}`)
+diaSemana = prompt("Qual é o dia da semana?");
+//converter tudo para respostas minusculas
+diaSemana = diaSemana.toLowerCase();
+if (diaSemana == "sabado" || diaSemana == "sábado" || diaSemana == "domingo"){
+    alert("Bom Final de Semana!!");
+}else{
+    alert("Boa Semana!!");
 }
+*/
+
+/*Desafio 2 - Alura
+numero = prompt("Digite um numero")
+if (numero > 0) {
+    alert(`O Numero que voce digitou é positivo: ${numero}`);
+}else{
+    alert(`O Numero que vocé digitou é negativo: ${numero}`)
+}
+*/
+
+/*Desafio 3 - Alura
+let nome = prompt("Digite Seu Nome")
+alert(`Bem-Vindo ao teste: ${nome}`)
+let saldo = 1000;
+alert(`Você Pode Ganhar: ${saldo} em dinheiro`)
+numero = prompt("Escolha um numero de 1 a 30");
+numeroSorte = 27
+let saldoAcumulado = 0;
+
+if (numero == numeroSorte){
+    alert(`Você Acertou o Numero da Sorte ${numeroSorte}`)
+    saldoAcumulado = saldoAcumulado + 1000;
+    alert(`Seu Saldo Atual é de: ${saldoAcumulado} Parabens Por Ganhar`)
+}else{
+    alert(`Você Errou o Numero da Sorte digitou: ${numero}`)
+}
+*/
