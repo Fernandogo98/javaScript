c32e2e304697234a8ad0d31b6e79a15afaa5744d
Fernandogo98 <nandojunior34@gmail.com>
2025-04-15 22:24:40 -0300

    V¨6

--- app.js
+++ app.js
@@ -92,8 +92,8 @@ while (numero >= contador) {
     contador++
 }*/
 
-/*
-Condição AND &&
+
+/*Condição AND &&
 idade = prompt("Digite Sua Idade para Acessar o Site");
 acesso = true;
 
@@ -102,5 +102,60 @@ if (idade >= 18 && acesso) {
 } else {
     alert(`Você é menor de idade, não pode acessar ainda: ${idade}`);
 }
-    */
-   
+*/
+/*Desafios finais
+Crie um programa que utilize o console.log para exibir uma mensagem de boas-vindas.*/
+
+// Crie uma variável chamada "nome" e atribua a ela o seu nome. Em seguida, utilize o console.log para exibir a mensagem "Olá, [seu nome]!" no console do navegador.
+let nome = prompt("Qual seu nome?");
+alert(`Ola, ${nome}`);
+let idade = prompt("Qual sua idade?");
+
+
+if (idade >= 18) {
+    alert(`${nome} Você pode continuar com a calculadora digite 2 numeros para somar:`);
+    let valor1 = Number(prompt("Digite o primeiro Valor"));
+    let valor2 = Number(prompt("Digite o segundo valor"));
+
+    let resultado = valor1 + valor2;
+    let divisao = valor1 / valor2;
+    let multi = valor1 * valor2;
+    let binario = resultado.toString(2);
+    while (binario.length % 4 !== 0) {
+        binario = "0" + binario;
+    }
+
+    let binarioFormatado = binario.match(/.{1,4}/g).join(" ");
+
+    let hexadecimal = resultado.toString(16);
+    let Octal = resultado.toString(8);
+
+    alert(`Soma: ${resultado}; Divisão: ${divisao}; Multiplicação: ${multi}; Binario: ${binarioFormatado}; Hexadecimal: ${hexadecimal}; Octal: ${Octal};`);
+    alert(resultado > 0 ? "O número é positivo." : resultado < 0 ? "O número é negativo." : "O número é zero.")
+} else {
+    alert(`Você ainda não tem idade para usar esta calculadora!!`);
+
+}
+
+
+// Crie uma variável chamada "nome" e atribua a ela o seu nome. Em seguida, utilize o alert para exibir a mensagem "Olá, [seu nome]!" .
+
+// Utilize o prompt e faça a seguinte pergunta: Qual a linguagem de programação que você mais gosta?. Em seguida, armazene a resposta em uma variável e mostre no console do navegador.
+
+// Crie uma variável chamada "valor1" e outra chamada "valor2", atribuindo a elas valores numéricos de sua escolha. Em seguida, realize a soma desses dois valores e armazene o resultado em uma terceira variável chamada "resultado". Utilize o console.log para mostrar a mensagem "A soma de [valor1] e [valor2] é igual a [resultado]." no console.
+
+// Crie uma variável chamada "valor1" e outra chamada "valor2", atribuindo a elas valores numéricos de sua escolha. Em seguida, realize a subtração desses dois valores e armazene o resultado em uma terceira variável chamada "resultado". Utilize o console.log para mostrar a mensagem "A diferença entre [valor1] e [valor2] é igual a [resultado]." no console.
+
+// Peça ao usuário para inserir sua idade com prompt. Com base na idade inserida, utilize um if para verificar se a pessoa é maior ou menor de idade, exibindo uma mensagem apropriada no console.
+
+// Crie uma variável "numero" e peça um valor com prompt verifique se é positivo, negativo ou zero. Use if-else para imprimir a respectiva mensagem.
+
+// Use um loop while para imprimir os números de 1 a 10 no console.
+
+// Crie uma variável "nota" e atribua um valor numérico a ela. Use if-else para determinar se a nota é maior ou igual a 7 e exiba "Aprovado" ou "Reprovado" no console.
+
+// Use o Math.random para gerar qualquer número aleatório e exiba esse número no console.
+
+// Use o Math.random para gerar um número inteiro entre 1 e 10 e exiba esse número no console.
+
+// Use o Math.random para gerar um número inteiro entre 1 e 1000 e exiba esse número no console.
